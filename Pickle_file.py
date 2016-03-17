cb0fe2b93ef0f25e100305ee5d1bd1c9432200e6
Korshun88 <skif989@rambler.ru>
2016-03-17 19:19:00 +0200

    1

--- Pickle_file.py
+++ Pickle_file.py
@@ -1,13 +1,16 @@
+import pickle
+import Model
+
 def load_f():
-try: 
-with open('file.txt') as file: 
-f = open( 'file.txt', "r" ) 
-BASE = pickle.load(f) 
-except IOError: 
-print ("Error! File not found or don`t read") 
-f.close()
+	try: 
+		with open('diary.txt') as file: 
+			f = open( 'diary.txt', "r" )
+			Model.diary = pickle.load(f)
+	except IOError: 
+		print ("Error! File not found or don`t read") 
+	f.close()
 
 def return_file():
-f = open( ‘file_out.txt’, ‘w’ )
-f.write( ‘??????’ )
-f.close()
+	f = open('diary.txt','w')
+	pickle.dump(Model.diary,f)
+	f.close()
