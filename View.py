ef67496ae78f63f595434617d3be5aea4481d4b6
dimakhupovetc <dhupovec@gmail.com>
2016-04-05 01:51:38 +0300

    Full edition

--- View.py
+++ View.py
@@ -1,5 +1,7 @@
 import Model
-import Pickle_file
+import Controller
+
+# function to display second menu
 
 
 def Main_menu():
@@ -10,56 +12,80 @@ def Main_menu():
     print ("                 5. FIND")
     print ("                 6. EXIT")
 
+# function to display second menu
+
+
 def Date_menu():
     print ("                 1. TEMPERATURE")
     print ("                 2. CLOUDS")
     print ("                 3. PRESSURE")
     print ("                 4. EXIT")
 
-#Pickle_file.load_f()
+# function to display all days
+
+
+def showAllDays(diary):
+    if (len(diary) == 0):
+        print ("Error! Diary is empty")
+    else:
+        for i in diary:
+            i.displayDayInfo()
+            print ("------------------------")
+
+# function to add a new day
+
+
+def addDay(diary, date, temperature, clouds, pressure):
+    for i in diary:
+        if i.date == date:
+            print("Error! Date already exists")
+    else:
+        diary.append(Model.CalendarDay(date, temperature, clouds, pressure))
+
 
 Main_menu()
+
 while True:
     key = raw_input()
     if key == "1":
-        Pickle_file.showAllDays()
+        showAllDays(Model.diary)
         Main_menu()
     elif key == "2":
         print ("Please enter data for the day")
         while True:
             print ("DATE (in dd.mm.yyyy): ")
             date = raw_input()
-            if (Pickle_file.checkingDate(date)):
+            if (Controller.checkingDate(date)):
                 break
 
         while True:
-            print ("TEMPERATURE (in Celsiuses): ")
+            print ("TEMPERATURE (in Celsiuses (-60 to +60)): ")
             temperature = raw_input()
-            if (Pickle_file.checkingTemperature(temperature)):
+            if (Controller.checkingTemperature(temperature)):
                 break
 
         while True:
             print ("CLOUDS (clear, sunny, cloudy, fog, rain): ")
             clouds = raw_input()
-            if (Pickle_file.checkingClouds(clouds)):
+            if (Controller.checkingClouds(clouds)):
                 break
 
         while True:
             print ("PRESSURE (value of 700 to 800): ")
             pressure = raw_input()
-            if (Pickle_file.checkingPressure(pressure)):
+            if (Controller.checkingPressure(pressure)):
                 break
-        Pickle_file.addDay(date, temperature, clouds, pressure)
+        addDay(Model.diary, date, temperature, clouds, pressure)
         date = None
         temperature = None
         clouds = None
         pressure = None
         Main_menu()
-    elif key =="3":
+    elif key == "3":
         while True:
             print ("DATE (in dd.mm.yyyy): ")
             date = raw_input()
-            if (Pickle_file.checkingDate(date)):
+            if (Controller.checkingDate(date)):
                 break
 
         for day in Model.diary:
@@ -71,10 +97,11 @@ while True:
         date = None
         Main_menu()
     elif key == "4":
+
         while True:
             print ("DATE (in dd.mm.yyyy): ")
             date = raw_input()
-            if (Pickle_file.checkingDate(date)):
+            if (Controller.checkingDate(date)):
                 break
 
         for day in Model.diary:
@@ -86,16 +113,16 @@ while True:
                         while True:
                             print ("TEMPERATURE (in Celsiuses): ")
                             temperature = raw_input()
-                            if (Pickle_file.checkingTemperature(temperature)):
+                            if (Controller.checkingTemperature(temperature)):
                                 break
                         day.setNewTemperature(temperature)
                         Date_menu()
                         temperature = None
                     elif key == "2":
                         while True:
-                            print ("CLOUDS (clear, sunny, cloudy, fog, rain): ")
+                            print ("CLOUDS (clear, sunny, cloudy, fog, rain):")
                             clouds = raw_input()
-                            if (Pickle_file.checkingClouds(clouds)):
+                            if (Controller.checkingClouds(clouds)):
                                 break
                         day.setNewClouds(clouds)
                         Date_menu()
@@ -104,7 +131,7 @@ while True:
                         while True:
                             print ("PRESSURE (value of 700 to 800): ")
                             pressure = raw_input()
-                            if (Pickle_file.checkingPressure(pressure)):
+                            if (Controller.checkingPressure(pressure)):
                                 break
                         day.setNewPressure(pressure)
                         Date_menu()
@@ -120,7 +147,7 @@ while True:
         while True:
             print ("DATE (in dd.mm.yyyy): ")
             date = raw_input()
-            if (Pickle_file.checkingDate(date)):
+            if (Controller.checkingDate(date)):
                 break
         for day in Model.diary:
             if day.date == date:
@@ -132,10 +159,7 @@ while True:
     elif key == "6":
         print ("Goodbye")
         break
-        
-        
 
-#Pickle_file.return_file()
-
-
-
+if (__name__ == '__main__'):
+    import doctest
+    doctest.testmod()
